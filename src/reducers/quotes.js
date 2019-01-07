e0cf11ba3eb066139c139659b8433ec9b761090e
no-relation <eddiec76@gmail.com>
2019-01-06 20:27:13 -0600

    why can't I find a quote with the ID?

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,40 @@
 export default (state = [], action) => {
-  return state;
+
+  let idx;
+
+  const getIndexWithID = (id) => {
+    return state.findIndex( quote => {
+      quote.id === id
+    })
+  }
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [ ...state, action.quote ]
+    break;
+
+    case "REMOVE_QUOTE":
+      idx = getIndexWithID(action.quoteId);
+      // why is idx undefined?
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+      break;
+    
+    case "UPVOTE_QUOTE":
+      idx = getIndexWithID(action.quoteId);
+      state[idx] = { ...state[idx], votes: state[idx].votes + 1}
+
+      return [ ...state ]
+    break;
+    
+    case "DOWNVOTE_QUOTE":
+      idx = getIndexWithID(action.quoteId);
+      state[idx] = { ...state[idx], votes: state[idx].votes - 1 }
+     
+      return [ ...state ]
+    break;
+  
+    default:
+      return state;
+    break;
+  }
 }
