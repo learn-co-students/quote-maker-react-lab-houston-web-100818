79b6969cd95c8d387b72e64b24276c574e3ae378
barbaradecares <barbaradecares@hotmail.com>
2019-01-08 09:58:10 -0600

    done

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,34 @@
-export default (state = [], action) => {
+function quotesReducer(state = [], action) {
+  const idx = state.indexOf(state.find(quote => quote.id === action.quoteId));
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+      break;
+    case "UPVOTE_QUOTE":
+      console.log(state);
+      let quotes = state;
+      let quote = state[idx];
+      quote.votes++;
+      return [...state.slice(0, idx), quote, ...state.slice(idx + 1)];
+      break;
+
+    case "DOWNVOTE_QUOTE":
+      if (state[idx].votes > 0) {
+        let quotes = state;
+        let quote = state[idx];
+        quote.votes--;
+        return [...state.slice(0, idx), quote, ...state.slice(idx + 1)];
+        break;
+      } else {
+        return state;
+      }
+
+      break;
+
+    case "REMOVE_QUOTE":
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+      break;
+  }
   return state;
 }
+export default quotesReducer;
